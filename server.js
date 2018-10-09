d5d5576b95b6797bbf251da59322003ae602ac78
Abbas1971 <radin2002@gmail.com>
2018-10-09 00:08:50 -0700

    second try changing the folders for deploy on heroku

--- server.js
+++ server.js
@@ -39,12 +39,10 @@ app.get("/", (req, res) => res.send("Hello hello"));
 // Server static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("../client/build"));
+  app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, "..", "client", "build", "index.html")
-    );
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
